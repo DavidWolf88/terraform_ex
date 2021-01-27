2fe9dae17a18c5eb1b67d93ca347cd4a9a080a18
Cloud User <centos@mail.sparta-szczekociny.pl>
2021-01-27 20:21:13 +0000

    security

--- main.tf
+++ main.tf
@@ -4,8 +4,25 @@ provider "aws" {
 resource "aws_instance" "example1" {
   ami           = "ami-01c74b7d55cbb6e31"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello moooooossss" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
     Name = "terraform-example"
+  }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
   }
 }
