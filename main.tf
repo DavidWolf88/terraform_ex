142dce5e9fe50f531d042cad782a8b32970ea37d
Dawid <dawid.wilk.30@gmail.com>
2021-01-27 22:08:17 +0100

    example fix

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-central-1"
 }
-resource "aws_instance" "example1" {
+resource "aws_instance" "example" {
   ami           = "ami-01c74b7d55cbb6e31"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
