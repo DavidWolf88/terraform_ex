dc622580b4fd5e43e11457b83349058e4e1ae4ea
Dawid <dawid.wilk.30@gmail.com>
2021-02-09 22:23:04 +0100

    chapter3

---
+++ chapter3/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "region = "eu-central-1
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "terraform_bucket"
+
+    #prevent acc deletion of this s3bucket
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    #full history of state files
+    versioning {
+        enabled = true
+    }
+    #server side encryption by default
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name         = "terraform_db"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
